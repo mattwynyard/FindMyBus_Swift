24e10b3f6e0fb2b841784ce9ef0aa971977f27e8
mattwynyard <mjwynyard@gmail.com>
2018-06-21 19:57:17 +1200

    Initial Commit

---
+++ FindMyBus/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FindMyBus
+//
+//  Created by Matthew Wynyard on 21/06/18.
+//  Copyright © 2018 Niobium. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
